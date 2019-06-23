8c5f623b01c6531b5321fff6f0dcaed262512962
ozaki <>
2019-06-24 01:10:35 +0900

    material設定追加

--- AnimeHairTools.py
+++ AnimeHairTools.py
@@ -28,13 +28,14 @@ NOTHING_ENUM = "(nothing)"  # noting selected item
 REMOVE_ENUM = "(remove setted object)"  # noting selected item
 
 # 3DView Tools Panel
-class MY_PT_ui(bpy.types.Panel):
+class ANIME_HAIR_TOOLS_PT_ui(bpy.types.Panel):
     bl_label = "Anime Hair Tools"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
   
     def draw(self, context):
-        self.layout.operator("anime_hair_tools.root_button")
+        self.layout.operator("anime_hair_tools.bevel_taper")
+        self.layout.operator("anime_hair_tools.material")
 
 
 # Bebel & Taper Setting
@@ -53,9 +54,8 @@ def create_curve_enum_items(self, context):
 
     return curve_enum
 
-
-class MY_OT_button(bpy.types.Operator):
-    bl_idname = "anime_hair_tools.root_button"
+class ANIME_HAIR_TOOLS_OT_bevel_taper(bpy.types.Operator):
+    bl_idname = "anime_hair_tools.bevel_taper"
     bl_label = "Bevel & Taper Setting"
 
     # create curve enum list
@@ -64,7 +64,7 @@ class MY_OT_button(bpy.types.Operator):
 
     # execute ok
     def execute(self, context):
-        # find all curves
+        # enum selected
         available_curves = get_available_curve_objects()
 
         # set bevel & taper to selected curves
@@ -98,11 +98,47 @@ class MY_OT_button(bpy.types.Operator):
         return context.window_manager.invoke_props_dialog(self)
 
 
+# Material Setting
+# *******************************************************************************************
+# find all curves for enum item
+def create_material_enum_items(self, context):
+    material_enum = []
+
+    for material in bpy.data.materials:
+        material_enum.append( (material.name, material.name, "") )
+
+    return material_enum
+
+class ANIME_HAIR_TOOLS_OT_material(bpy.types.Operator):
+    bl_idname = "anime_hair_tools.material"
+    bl_label = "Material Setting"
+
+    # create material enum list
+    selected_material: bpy.props.EnumProperty(name="Material", items=create_material_enum_items)
+
+    # execute ok
+    def execute(self, context):
+        # set material to selected curves
+        selected_curves = get_selected_curve_objects()
+        for curve_name in selected_curves:
+            curve = selected_curves[curve_name]
+            curve.data.materials[curve.active_material_index] = bpy.data.materials[self.selected_material]
+
+
+
+        return{'FINISHED'}
+
+    # use dialog
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+
 # retister blender
 # *******************************************************************************************
 classes = (
-    MY_PT_ui,
-    MY_OT_button
+    ANIME_HAIR_TOOLS_PT_ui,
+    ANIME_HAIR_TOOLS_OT_bevel_taper,
+    ANIME_HAIR_TOOLS_OT_material,
 )
 
 for cls in classes:
