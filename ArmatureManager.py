be27818f1376fb122a0b744296c96818d265fce4
ozaki <jp.ibis@gmail.com>
2020-10-03 22:47:15 +0900

    コメントの修正

--- ArmatureManager.py
+++ ArmatureManager.py
@@ -86,11 +86,15 @@ def ui_draw(context, layout):
     box.operator("anime_hair_tools.setup_armature")
 
 
+# コンストレイント先リストデータ
+# =================================================================================================
+# コンストレイント先のArmatureの候補
 def get_armature_list(self, context):
     armature_list = [(obj.name, obj.name, "") for obj in context.scene.objects if obj.type == "ARMATURE" and obj.name != context.scene.AHT_armature_name]
     armature_list.insert(0, ("_empty_for_delete", "", ""))  # 空も設定できるように
     return armature_list
 
+# コンストレイント先のBoneの候補
 def get_bone_list(self, context):
     scene = context.scene
 
@@ -98,15 +102,15 @@ def get_bone_list(self, context):
     if scene.AHT_constraint_target_name.armature == "_empty_for_delete":
         return []
 
+    # 選択されているarmatureのboneをリストする
     armature = bpy.data.objects[context.scene.AHT_constraint_target_name.armature]
     return [(bone.name, bone.name, "") for bone in armature.data.bones]
 
-
-
-# コンストレイント先
+# コンストレイント先データ
 class ConstraintTargetProperty(bpy.types.PropertyGroup):
     armature: bpy.props.EnumProperty(items=get_armature_list)
     bone: bpy.props.EnumProperty(items=get_bone_list)
+
 
 # =================================================================================================
 def register():
