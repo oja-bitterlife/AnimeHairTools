670b9af34f74affd1ef1a76a04617610a55fccb4
ozaki <jp.ibis@gmail.com>
2020-10-05 18:12:16 +0900

    モディファイアの順番整理

--- ChildBoneAndHook_Util/HookManager.py
+++ ChildBoneAndHook_Util/HookManager.py
@@ -15,7 +15,7 @@ def create(context, selected_curve_objs):
         # spline単位で処理
         for spline_no, spline in enumerate(curve_obj.data.splines):
             # 頂点ごとにHookを作成する
-            for target_bone_no in range(len(spline.points)-1):
+            for target_bone_no in reversed(range(len(spline.points)-1)):
                 hook_name = Naming.make_modifier_name(curve_obj.name, spline_no, target_bone_no)
 
                 # create modifier
@@ -32,6 +32,23 @@ def create(context, selected_curve_objs):
 
             # hook用のpointのindexが取れないので、計算で出してみる用
             hook_offset += len(spline.points)
+
+
+        # hook_modifire以外を後ろに持っていく
+        # *********************************************************************
+        # 処理するCurveをActiveにしておく必要があるっぽい
+        bpy.context.view_layer.objects.active = curve_obj
+
+        # Hookモディファイアでなければ、モディファイアの数だけ下に移動する
+        hook_basename = Naming.make_modifier_basename(curve_obj.name) + Naming.HOOK_MODIFIRE_SEPALATER
+        for modifier in curve_obj.modifiers:
+            # Hookモディファイアを見つけたらそこで終了
+            if modifier.name.startswith(hook_basename):
+                break
+
+            # Hookモディファイアでない間は下に移動
+            for i in range(len(curve_obj.modifiers)-1):
+                bpy.ops.object.modifier_move_down(modifier=modifier.name)
 
 
 # Hookの削除
