3a9bccf318aebd0901c89198b8549825d07209a1
ozaki <jp.ibis@gmail.com>
2022-03-02 18:54:58 +0900

    バージョンを0.2.0に

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "oja",
     "description" : "",
     "blender" : (3, 0, 0),
-    "version" : (0, 1, 0),
+    "version" : (0, 2, 0),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
