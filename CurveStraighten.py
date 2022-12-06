5aabc969e0ebdd47960086b401b2c06f9c79a1e5
oja-bitterlife <bitter.life.club@gmail.com>
2022-12-06 17:09:53 +0900

    ストレート化部分を関数に分離

--- CurveStraighten.py
+++ CurveStraighten.py
@@ -10,53 +10,59 @@ class ANIME_HAIR_TOOLS_OT_curve_straighten(bpy.types.Operator):
     # execute ok
     def execute(self, context):
         selected_curve_objs = [obj for obj in context.selected_objects if obj.type == "CURVE"]
+        err = self.do_straighten(context, selected_curve_objs)
 
-        for curve in selected_curve_objs:
-            for spline in curve.data.splines:
-                if spline.type == "NURBS":
-                    self.execute_nurbs(context, curve, spline)
-                elif spline.type == "BEZIER":
-                    self.report({'ERROR'}, "unsupported type: %s" % spline.type)
-                else:
-                    self.report({'ERROR'}, "unknown type: %s" % spline.type)
-                    return {'CANCELLED'}
+        if err != "":
+            self.report({'ERROR'}, err)
+            return {'CANCELLED'}
 
         return {'FINISHED'}
 
-    def execute_nurbs(self, context, curve, spline):
-        # Curveを移動させると移動前のポイントとの長さが変わるので、先に長さだけ抜き出しておく
-        length_list = []
-        for i, point in enumerate(spline.points):
-            if i == 0:
-                length_list.append(0)
-            else:
-                length_list.append((spline.points[i].co.xyz - spline.points[i-1].co.xyz).length)
+    def do_straighten(self, context, selected_curve_objs):
+        for curve in selected_curve_objs:
+            for spline in curve.data.splines:
+                if spline.type == "NURBS":
+                    self.execute_nurbs_straighten(spline, context.scene.AHT_straighten_keep_length)
+                elif spline.type == "BEZIER":
+                    return "unsupported type: %s" % spline.type
+                else:
+                    return "unknown type: %s" % spline.type
+        return ""
 
-        nvec = None
-        for i, point in enumerate(spline.points):
-            # まだ方向が未定(選択点を見つけていない)
-            if nvec == None:
-                if point.select:
-                    # 方向取得
-                    if i == 0:
-                        # 最初が選択されてるときは次のポイントとの直線
-                        nvec = (spline.points[i+1].co.xyz - point.co.xyz).normalized()
-                    elif i == len(length_list)-1:
-                        break  # 最後が選択されてるときは何もしない
-                    else:
-                        # 前のポイントとの直線
-                        nvec = (point.co.xyz - spline.points[i-1].co.xyz).normalized()
+def execute_nurbs_straighten(spline, keep_length):
+    # Curveを移動させると移動前のポイントとの長さが変わるので、先に長さだけ抜き出しておく
+    length_list = []
+    for i, point in enumerate(spline.points):
+        if i == 0:
+            length_list.append(0)
+        else:
+            length_list.append((spline.points[i].co.xyz - spline.points[i-1].co.xyz).length)
 
-                    # keep length じゃなければconstant化
-                    if not context.scene.AHT_straighten_keep_length:
-                        total_length = sum(length_list[i+1:])
-                        constant_length = total_length / (len(length_list)-(i+1))
-                        for j in range(i+1, len(length_list)):
-                            length_list[j] = constant_length
+    vec = None
+    for i, point in enumerate(spline.points):
+        # まだ方向が未定(選択点を見つけていない)
+        if vec == None:
+            if point.select:
+                # 方向取得
+                if i == 0:
+                    # 最初が選択されてるときは次のポイントとの直線
+                    vec = (spline.points[i+1].co.xyz - point.co.xyz).normalized()
+                elif i == len(length_list)-1:
+                    break  # 最後が選択されてるときは何もしない
+                else:
+                    # 前のポイントとの直線
+                    vec = (point.co.xyz - spline.points[i-1].co.xyz).normalized()
 
-            else:
-                # 選択点以降はまっすぐに配置しなおす
-                spline.points[i].co.xyz = spline.points[i-1].co.xyz + (nvec * length_list[i])
+                # keep length じゃなければconstant化
+                if not keep_length:
+                    total_length = sum(length_list[i+1:])
+                    constant_length = total_length / (len(length_list)-(i+1))
+                    for j in range(i+1, len(length_list)):
+                        length_list[j] = constant_length
+
+        else:
+            # 選択点以降はまっすぐに配置しなおす
+            spline.points[i].co.xyz = spline.points[i-1].co.xyz + (vec * length_list[i])
 
 
 # UI描画設定
