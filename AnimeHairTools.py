932434abc192ea4165e48fad03ce80134d115a6d
oja-bitterlife <bitter.life.club@gmail.com>
2023-01-28 22:41:15 +0900

    カテゴリ修正

--- AnimeHairTools.py
+++ AnimeHairTools.py
@@ -13,7 +13,7 @@ class ANIME_HAIR_TOOLS_PT_ui(bpy.types.Panel):
     bl_label = "Anime Hair Tools"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_category = "AnimeHairTools"
+    bl_category = "AnimeTools"
 
     def draw(self, context):
         # 状態によって使うUIを切り替える
--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ bl_info = {
     "version" : (0, 2, 0),
     "location" : "",
     "warning" : "",
-    "category" : "Generic"
+    "category" : "Pose"
 }
 
 from . import auto_load
