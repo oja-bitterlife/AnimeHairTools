e6b761a87331569cfffeaaaf24a7dcbf516b0baa
oja-bitterlife <bitter.life.club@gmail.com>
2024-09-29 00:07:35 +0900

    ボーン生成修正

--- CreateHair/ArmatureSetup.py
+++ CreateHair/ArmatureSetup.py
@@ -1,7 +1,6 @@
 import bpy, mathutils
 import math
 
-from . import BoneManager
 from ..Util import Naming
 
 # AHT用のArmatureのセットアップを行う
@@ -14,8 +13,11 @@ class ANIME_HAIR_TOOLS_OT_setup_armature(bpy.types.Operator):
     def execute(self, context):
         scene = context.scene
 
-        parent_armature = bpy.data.objects[scene.AHT_parent_armature_name]
-        parent_bone = parent_armature.data.bones[scene.AHT_parent_bone_name]
+        parent_armature = bpy.data.objects.get(scene.AHT_parent_armature_name)
+        if parent_armature == None:
+            parent_bone = None
+        else:
+            parent_bone = parent_armature.data.bones.get(scene.AHT_parent_bone_name)
 
         # armature
         # -------------------------------------------------------------------------
@@ -27,22 +29,26 @@ class ANIME_HAIR_TOOLS_OT_setup_armature(bpy.types.Operator):
 
         armature.parent = parent_armature
 
-        # root_bone
-        # -------------------------------------------------------------------------
-        armature.data.bones[0].name = scene.AHT_root_bone_name  # 名前を修正
-        armature.parent = parent_armature
-        armature.parent_type = 'BONE'
-        armature.parent_bone = parent_bone.name
-
-        # 設定
+        # root_bone設定
         # -------------------------------------------------------------------------
         context.view_layer.objects.active = armature
 
-        # 座標設定
-        bpy.ops.object.mode_set(mode='EDIT')
-        armature.data.edit_bones[0].head = parent_bone.head
-        armature.data.edit_bones[0].tail = parent_bone.head + mathutils.Vector((0, 0, -0.5))
-        bpy.ops.object.mode_set(mode='OBJECT')
+        # 名前
+        armature.data.bones[0].name = scene.AHT_root_bone_name
+
+        # 親子付け
+        if parent_armature != None:
+            armature.parent = parent_armature
+            armature.parent_type = 'BONE'
+        if parent_bone != None:
+            armature.parent_bone = parent_bone.name
+
+        # 座標
+        if parent_bone != None:
+            bpy.ops.object.mode_set(mode='EDIT')
+            armature.data.edit_bones[0].head = parent_bone.head + mathutils.Vector((0, 0, -0.5))
+            armature.data.edit_bones[0].tail = parent_bone.head
+            bpy.ops.object.mode_set(mode='OBJECT')
 
         return{'FINISHED'}
 
@@ -69,64 +75,44 @@ class ANIME_HAIR_TOOLS_OT_setup_armature(bpy.types.Operator):
         armature.show_in_front = True  # 常に手前
         armature.data.display_type = 'WIRE'  # WIRE表示
 
-        # 見やすいように奥向きに設定しておく
-        # -------------------------------------------------------------------------
-        # bpy.ops.object.select_all(action='DESELECT')
-        # bpy.context.view_layer.objects.active = armature
-        # bpy.ops.object.mode_set(mode='EDIT')
-        # armature.data.edit_bones[0].select = True
-        # armature.data.edit_bones[0].tail = (0, 1, 0)
-        # bpy.ops.object.mode_set(mode='POSE')
-        # bpy.context.active_pose_bone.rotation_mode = 'XYZ'
-        # bpy.ops.object.mode_set(mode='OBJECT')
-
         # 初回限定処理
         # -------------------------------------------------------------------------
         armature.data.collections[0].name = "AHT_Root"
-
-        # Armatureコンストレイントも用意しておく
-        constraint = armature.pose.bones[0].constraints.new("ARMATURE")
-        constraint.name = Naming.make_constraint_name("parent")
 
         return armature
 
 
 # create constraints and controll bone
 # =================================================================================================
-class ANIME_HAIR_TOOLS_OT_create(bpy.types.Operator):
+class ANIME_HAIR_TOOLS_OT_create_bones(bpy.types.Operator):
     bl_idname = "anime_hair_tools.create"
-    bl_label = "Create Mesh & Bones"
+    bl_label = "Create Bones"
 
     # execute ok
     def execute(self, context):
         armature = bpy.data.objects[context.scene.AHT_armature_name]
-        selected_curve_objs = [obj for obj in context.selected_objects if obj.type == "CURVE"]
-        backup_active = bpy.context.view_layer.objects.active
+        # selected_curve_objs = [obj for obj in context.selected_objects if obj.type == "CURVE"]
+        # backup_active = bpy.context.view_layer.objects.active
 
-        # Curveが１つも選択されていなかった
-        if len(selected_curve_objs) == 0:
-            return{'FINISHED'}
+        # # Curveが１つも選択されていなかった
+        # if len(selected_curve_objs) == 0:
+        #     return{'FINISHED'}
 
-        # 一旦今までのものを削除
-        # ---------------------------------------------------------------------
-        BoneManager.remove(context, selected_curve_objs)  # Boneを削除
-        MeshManager.remove(context, selected_curve_objs)  # Meshも削除
+        # # 一旦今までのものを削除
+        # # ---------------------------------------------------------------------
+        # BoneManager.remove(context, selected_curve_objs)  # Boneを削除
 
-        # 作り直す
-        # ---------------------------------------------------------------------
-        # create mesh
-        meshed_curve_list_group = MeshManager.create(context, selected_curve_objs)
-        # create bones
-        BoneManager.create(context, selected_curve_objs, meshed_curve_list_group)
-        # JOIN & 名前設定
-        MeshManager.join_and_settings(selected_curve_objs, meshed_curve_list_group)
+        # # 作り直す
+        # # ---------------------------------------------------------------------
+        # # create bones
+        # BoneManager.create(context, selected_curve_objs, meshed_curve_list_group)
 
-        # 後始末
-        # ---------------------------------------------------------------------
-        bpy.ops.object.select_all(action='DESELECT')
-        for curve in selected_curve_objs:  # 対象となったCurveを選択状態に戻しておく
-            curve.select_set(True)
-        bpy.context.view_layer.objects.active = backup_active
+        # # 後始末
+        # # ---------------------------------------------------------------------
+        # bpy.ops.object.select_all(action='DESELECT')
+        # for curve in selected_curve_objs:  # 対象となったCurveを選択状態に戻しておく
+        #     curve.select_set(True)
+        # bpy.context.view_layer.objects.active = backup_active
 
         return{'FINISHED'}
 
@@ -141,30 +127,30 @@ class ANIME_HAIR_TOOLS_OT_create(bpy.types.Operator):
 # =================================================================================================
 class ANIME_HAIR_TOOLS_OT_remove(bpy.types.Operator):
     bl_idname = "anime_hair_tools.remove"
-    bl_label = "Remove Mesh & Bones"
+    bl_label = "Remove Bones"
 
     # execute ok
     def execute(self, context):
-        backup_active = bpy.context.view_layer.objects.active
+        # backup_active = bpy.context.view_layer.objects.active
 
-        selected_curve_objs = [obj for obj in context.selected_objects if obj.type == "CURVE"]
-        # Curveが１つも選択されていなかった
-        if len(selected_curve_objs) == 0:
-            return{'FINISHED'}
+        # selected_curve_objs = [obj for obj in context.selected_objects if obj.type == "CURVE"]
+        # # Curveが１つも選択されていなかった
+        # if len(selected_curve_objs) == 0:
+        #     return{'FINISHED'}
 
-        # remove child bones
-        if bpy.data.objects.get(context.scene.AHT_armature_name) != None:  # ATHのArmatureがあるときだけBoneを消せる
-            BoneManager.remove(context, selected_curve_objs)  # ボーンを削除
+        # # remove child bones
+        # if bpy.data.objects.get(context.scene.AHT_armature_name) != None:  # ATHのArmatureがあるときだけBoneを消せる
+        #     BoneManager.remove(context, selected_curve_objs)  # ボーンを削除
 
-        # remove mesh
-        MeshManager.remove(context, selected_curve_objs)
+        # # remove mesh
+        # MeshManager.remove(context, selected_curve_objs)
 
-        # 後始末
-        # ---------------------------------------------------------------------
-        bpy.ops.object.select_all(action='DESELECT')
-        for curve in selected_curve_objs:  # 対象となったCurveを選択状態に戻しておく
-            curve.select_set(True)
-        bpy.context.view_layer.objects.active = backup_active
+        # # 後始末
+        # # ---------------------------------------------------------------------
+        # bpy.ops.object.select_all(action='DESELECT')
+        # for curve in selected_curve_objs:  # 対象となったCurveを選択状態に戻しておく
+        #     curve.select_set(True)
+        # bpy.context.view_layer.objects.active = backup_active
 
         return{'FINISHED'}
 
@@ -173,40 +159,9 @@ class ANIME_HAIR_TOOLS_OT_remove(bpy.types.Operator):
 # =================================================================================================
 classes = [
     ANIME_HAIR_TOOLS_OT_setup_armature,
-    ANIME_HAIR_TOOLS_OT_create,
+    ANIME_HAIR_TOOLS_OT_create_bones,
     ANIME_HAIR_TOOLS_OT_remove,
 ]
-
-def draw(parent, context, layout):
-    # Armature生成
-    # ---------------------------------------------------------------------
-    layout.label(text="Armature and RootBone Setting:")
-    box = layout.box()
-    box.enabled = context.mode == "OBJECT"
-
-    # ATHのArmatureの設定
-    box.prop(context.scene, "AHT_armature_name", text="Armature")
-    box.prop(context.scene, "AHT_root_bone_name", text="RootBone")
-    box.prop(context.scene, "AHT_parent_armature_name", text="ParentArmature")
-    row = box.row()
-    row.prop(context.scene, "AHT_parent_bone_group", text="ParentBone")
-    row.prop(context.scene, "AHT_parent_bone_name", text="")
-
-    # 実行
-    box.operator("anime_hair_tools.setup_armature")
-
-    # Mesh化&Bone生成
-    # ---------------------------------------------------------------------
-    layout.label(text="Mesh & Bones Setting:")
-    box = layout.box()
-    box.enabled = context.mode == "OBJECT" and (bpy.context.view_layer.objects.active != None and bpy.context.view_layer.objects.active.type == "CURVE")
-
-    box.prop(context.scene, "AHT_subdivision", text="Use Subdivision Modifire")
-    box.prop(context.scene, "AHT_bbone", text="Bendy Bones")
-    box.prop(context.scene, "AHT_create_layer", text="Bone Create Layer")
-    box.operator("anime_hair_tools.create")
-    box.operator("anime_hair_tools.remove")
-
 
 def register():
     for cls in classes:
--- UI_SetupArmature.py
+++ UI_SetupArmature.py
@@ -1,8 +1,9 @@
 import bpy
-from .CreateHair import ArmatureSetup
+from .CreateHair import ArmatureSetup, BoneManager
 
 modules = [
     ArmatureSetup,
+    BoneManager,
 ]
 
 class ANIME_HAIR_TOOLS_PT_setup_armature(bpy.types.Panel):
@@ -14,11 +15,32 @@ class ANIME_HAIR_TOOLS_PT_setup_armature(bpy.types.Panel):
     bl_options = {'DEFAULT_CLOSED'}
 
     def draw(self, context):
-        for i, module in enumerate(modules):
-            if hasattr(module, "draw"):
-                if hasattr(module, "label"):
-                    self.layout.label(text=module.label)
-                module.draw(self, context, self.layout.box())
+        # Armature生成
+        # ---------------------------------------------------------------------
+        self.layout.label(text="Armature and RootBone Setting:")
+        box = self.layout.box()
+        box.enabled = context.mode == "OBJECT"
+
+        # ATHのArmatureの設定
+        box.prop(context.scene, "AHT_armature_name", text="Armature")
+        box.prop(context.scene, "AHT_root_bone_name", text="RootBone")
+        box.prop(context.scene, "AHT_parent_armature_name", text="ParentArmature")
+        row = box.row()
+        row.prop(context.scene, "AHT_parent_bone_group", text="ParentBone")
+        row.prop(context.scene, "AHT_parent_bone_name", text="")
+
+        # 実行
+        box.operator("anime_hair_tools.setup_armature")
+
+        # Mesh化&Bone生成
+        # ---------------------------------------------------------------------
+        self.layout.label(text="Bones Setting:")
+        box = self.layout.box()
+        box.enabled = context.mode == "OBJECT" and (bpy.context.view_layer.objects.active != None and bpy.context.view_layer.objects.active.type == "CURVE")
+
+        box.prop(context.scene, "AHT_create_layer", text="Create Layer")
+        box.operator("anime_hair_tools.create")
+        box.operator("anime_hair_tools.remove")
 
 
 # Enumrateの設定
@@ -47,6 +69,8 @@ def register():
     bpy.types.Scene.AHT_parent_armature_name = bpy.props.EnumProperty(name = "parent armature name", items=get_armature_names)
     bpy.types.Scene.AHT_parent_bone_group = bpy.props.StringProperty(name = "parent bone group", default="Bones")
     bpy.types.Scene.AHT_parent_bone_name = bpy.props.EnumProperty(name = "parent bone name", items=get_bone_names)
+
+    bpy.types.Scene.AHT_create_layer = bpy.props.StringProperty(name = "bone create layer", default="AHT_HairBones")
 
 def unregister():
     for module in modules:
